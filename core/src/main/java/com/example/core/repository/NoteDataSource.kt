6d1da5b82d56ef8c487a1a23f3b0774bf1c8070a
mojtabagituser <m.heydarpanah@gmail.com>
2022-11-22 21:49:34 -0500

    repository is created

---
+++ core/src/main/java/com/example/core/repository/NoteDataSource.kt
@@ -0,0 +1,11 @@
+package com.example.core.repository
+
+import com.example.core.data.Note
+
+interface NoteDataSource {
+
+    suspend fun add(note: Note)
+    suspend fun get(id:Long):Note?
+    suspend fun getAll():List<Note>
+    suspend fun delete(note: Note)
+}
